90becd34e389be8c81447c8dd4729a76bea4351e
Josh Carrico <jcarrico138@gmail.com>
2015-03-23 09:14:25 -0400

    angular frameworks

--- index.js
+++ index.js
@@ -6,7 +6,29 @@ sportsApp.controller("HeaderController", ["$scope", "getData", function($scope, 
 
 		$scope.data = data
 
-		console.log($scope)
+	});
+
+}]);
+
+sportsApp.controller("HeadlineController", ["$scope", "getData", function($scope, getData){
+
+	getData("http://tiy-espn-info.herokuapp.com/espn/carousel", function(data){
+
+	$scope.data = data
+
+	console.log($scope)
+
+	});
+
+}]);
+
+sportsApp.controller("HighlightsController", ["$scope", "getData", function($scope, getData){
+
+	getData("http://tiy-espn-info.herokuapp.com/espn/highlights", function(data){
+
+	$scope.data = data
+
+	console.log($scope)
 
 	});
 
